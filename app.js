be96b174666a25a43c34132ad885216811acb765
cudaman73 <dale.evans.3@gmail.com>
2022-06-30 12:10:10 -0500

    moves boilerplate html to header/footer templates

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let tasks = [];
+let workTasks = [];
 app.use(express.static('css'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,14 +14,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
   let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   let day = daysOfWeek[new Date().getDay()]
-  res.render('list', {day: day, tasks: tasks})
+  res.render('list', {listTitle: day, tasks: tasks})
+});
+
+app.get("/work", function(req, res) {
+  res.render('list', {listTitle: "Work", tasks: workTasks})
+});
+
+app.get("/about", function(req, res) {
+  res.render('about')
 });
 
 app.post("/", function(req, res) {
+  if (req.body.addButton === "Work") {
+    workTasks.push(req.body.newTask)
+    res.redirect("/work")
+  } else {
   tasks.push(req.body.newTask)
   res.redirect("/")
+}
 });
 
+// app.post("/work", function(req, res) {
+//   console.log(req.body)
+// });
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
