6b2b3b86870df0d1354079d030ecf2cb174df640
cudaman73 <dale.evans.3@gmail.com>
2022-07-02 07:29:53 -0500

    Adds mongoDB task list storage

--- app.js
+++ app.js
@@ -2,42 +2,96 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-
-let tasks = [];
-let workTasks = [];
 app.use(express.static('css'))
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect(
+  "mongodb+srv://dbUser:Px0paLYznaE2QjJ7@cluster0.pokrv.mongodb.net/todolistDB?retryWrites=true&w=majority"
+);
+
+const itemsSchema = {
+  name: {
+    type: String,
+    required: true
+  }
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const defaultItems = [new Item({
+    name: "Welcome to your new Todo List!"
+  }),
+  new Item({
+    name: "Click + to add a new task"
+  }),
+  new Item({
+    name: "<-- Check this when you're done"
+  })
+];
+
 
 app.get("/", function(req, res) {
-  let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  let day = daysOfWeek[new Date().getDay()]
-  res.render('list', {listTitle: day, tasks: tasks})
-});
-
-app.get("/work", function(req, res) {
-  res.render('list', {listTitle: "Work", tasks: workTasks})
+  Item.find({}, (err, tasks) => {
+    if (tasks.length === 0) {
+      Item.insertMany(defaultItems, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Items saved to dB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render('list', {
+        listTitle: "Today's",
+        tasks: tasks
+      });
+    }
+  });
 });
 
 app.get("/about", function(req, res) {
-  res.render('about')
+  res.render('about');
 });
 
 app.post("/", function(req, res) {
   if (req.body.addButton === "Work") {
-    workTasks.push(req.body.newTask)
-    res.redirect("/work")
+    workTasks.push(req.body.newTask);
+    res.redirect("/work");
   } else {
-  tasks.push(req.body.newTask)
-  res.redirect("/")
-}
+    Item.insertMany({
+      name: req.body.newTask
+    }, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Item saved to db");
+      }
+    });
+    res.redirect("/");
+  }
 });
 
-// app.post("/work", function(req, res) {
-//   console.log(req.body)
-// });
+app.post("/delete", function(req, res) {
+  Item.deleteOne({
+    _id: req.body._id
+  }, error => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Item deleted from db");
+    }
+  })
+  res.redirect("/")
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
+
+//
